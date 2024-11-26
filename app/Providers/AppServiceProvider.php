f4757686b134478eb1b6cd54e2ad136fba4dde73
David Vigil <vigilgarcia98@gmail.com>
2024-11-26 10:51:10 -0600

    Modulo de autenticacion agregado

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Interfaces\IAuthRepository;
+use App\Interfaces\IAuthService;
 use App\Interfaces\ICategoryRepository;
 use App\Interfaces\ICategoryService;
 use App\Interfaces\IProductRepository;
@@ -10,10 +12,12 @@ use App\Interfaces\IRoleRepository;
 use App\Interfaces\IRoleService;
 use App\Interfaces\IUserRepository;
 use App\Interfaces\IUserService;
+use App\Repositories\AuthRepository;
 use App\Repositories\CategoryRepository;
 use App\Repositories\ProductRepository;
 use App\Repositories\RoleRepository;
 use App\Repositories\UserRepository;
+use App\Services\AuthService;
 use App\Services\CategoryService;
 use App\Services\ProductService;
 use App\Services\RoleService;
@@ -32,12 +36,14 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(IProductRepository::class, ProductRepository::class);
         $this->app->bind(IRoleRepository::class, RoleRepository::class);
         $this->app->bind(IUserRepository::class, UserRepository::class);
+        $this->app->bind(IAuthRepository::class, AuthRepository::class);
 
         // Services
         $this->app->bind(ICategoryService::class, CategoryService::class);
         $this->app->bind(IProductService::class, ProductService::class);
         $this->app->bind(IRoleService::class, RoleService::class);
         $this->app->bind(IUserService::class, UserService::class);
+        $this->app->bind(IAuthService::class, AuthService::class);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
@@ -17,34 +18,43 @@ use App\Http\Controllers\UserController;
 |
 */
 
-// categories
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::post('/categories', [CategoryController::class, 'store']);
-Route::get('/categories/{id}', [CategoryController::class, 'show']);
-Route::put('/categories/{id}', [CategoryController::class, 'update']);
-Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
-Route::get('/categories/{critery}/{value}/search', [CategoryController::class, 'search']);
+// login
+Route::post('/login', [AuthController::class, 'login']);
 
-// products
-Route::get('/products', [ProductController::class, 'index']);
-Route::post('/products', [ProductController::class, 'store']);
-Route::get('/products/{id}', [ProductController::class, 'show']);
-Route::put('/products/{id}', [ProductController::class, 'update']);
-Route::delete('/products/{id}', [ProductController::class, 'destroy']);
-Route::get('/products/{critery}/{value}/search', [ProductController::class, 'search']);
+Route::middleware('jwt.verify')->group(function () {
 
-// roles
-Route::get('/roles', [RoleController::class, 'index']);
-Route::post('/roles', [RoleController::class, 'store']);
-Route::get('/roles/{id}', [RoleController::class, 'show']);
-Route::put('/roles/{id}', [RoleController::class, 'update']);
-Route::delete('/roles/{id}', [RoleController::class, 'destroy']);
-Route::get('/roles/{critery}/{value}/search', [RoleController::class, 'search']);
+    // logout
+    Route::get('/logout', [AuthController::class, 'logout']);
 
-// users
-Route::get('/users', [UserController::class, 'index']);
-Route::post('/users', [UserController::class, 'store']);
-Route::get('/users/{id}', [UserController::class, 'show']);
-Route::put('/users/{id}', [UserController::class, 'update']);
-Route::delete('/users/{id}', [UserController::class, 'destroy']);
-Route::get('/users/{critery}/{value}/search', [UserController::class, 'search']);
+    // categories
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::post('/categories', [CategoryController::class, 'store']);
+    Route::get('/categories/{id}', [CategoryController::class, 'show']);
+    Route::put('/categories/{id}', [CategoryController::class, 'update']);
+    Route::delete('/categories/{id}', [CategoryController::class, 'destroy']);
+    Route::get('/categories/{critery}/{value}/search', [CategoryController::class, 'search']);
+
+    // products
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::get('/products/{id}', [ProductController::class, 'show']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+    Route::get('/products/{critery}/{value}/search', [ProductController::class, 'search']);
+
+    // roles
+    Route::get('/roles', [RoleController::class, 'index']);
+    Route::post('/roles', [RoleController::class, 'store']);
+    Route::get('/roles/{id}', [RoleController::class, 'show']);
+    Route::put('/roles/{id}', [RoleController::class, 'update']);
+    Route::delete('/roles/{id}', [RoleController::class, 'destroy']);
+    Route::get('/roles/{critery}/{value}/search', [RoleController::class, 'search']);
+
+    // users
+    Route::get('/users', [UserController::class, 'index']);
+    Route::post('/users', [UserController::class, 'store']);
+    Route::get('/users/{id}', [UserController::class, 'show']);
+    Route::put('/users/{id}', [UserController::class, 'update']);
+    Route::delete('/users/{id}', [UserController::class, 'destroy']);
+    Route::get('/users/{critery}/{value}/search', [UserController::class, 'search']);
+});
